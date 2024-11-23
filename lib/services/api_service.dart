38fe36093a0a41c2e7c3a1010cbcd4247122b999
HayHay <dev@hayhay.dev>
2024-11-23 10:36:13 -0500

    Formatting

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,23 +1,25 @@
-import 'package:frontend_template/utils/app_config.dart';
-import 'package:http/http.dart' as http;
-import 'package:supabase_flutter/supabase_flutter.dart';
+import "dart:convert";
+
+import "package:frontend_template/utils/app_config.dart";
+import "package:http/http.dart" as http;
+import "package:supabase_flutter/supabase_flutter.dart";
 
 class ApiService {
   late final String baseUrl;
-  final accessToken = Supabase.instance.client.accessToken;
+  final accessToken = Supabase.instance.client.auth.currentSession?.accessToken;
 
   ApiService() {
     baseUrl = AppConfig.apiBaseUrl;
   }
 
-  // Basic ping that doesn't need auth
+  // Basic ping that doesn"t need auth
   Future<String> ping() async {
     try {
-      final response = await http.get(Uri.parse('$baseUrl/ping'));
+      final response = await http.get(Uri.parse("$baseUrl/ping"));
       if (response.statusCode == 200) {
         return response.body;
       }
-      throw 'Failed to ping';
+      throw "Failed to ping";
     } catch (e) {
       rethrow;
     }
@@ -26,19 +28,25 @@ class ApiService {
   // Authenticated ping that needs bearer token
   Future<String> pingAuthenticated() async {
     try {
-      if (accessToken == null) throw 'No auth token';
+      final accessToken =
+          Supabase.instance.client.auth.currentSession?.accessToken;
+      if (accessToken == null) throw "No auth token";
 
       final response = await http.get(
-        Uri.parse('$baseUrl/ping-authenticated'),
+        Uri.parse("$baseUrl/ping-authenticated"),
         headers: {
-          'Authorization': 'Bearer $accessToken',
+          "Authorization": "Bearer $accessToken",
         },
       );
 
       if (response.statusCode == 200) {
-        return response.body;
+        final jsonResponse = await json.decode(response.body);
+        return {
+          "message": jsonResponse["message"],
+          "user": jsonResponse["user"]["email"],
+        }.toString();
       }
-      throw 'Failed to ping authenticated endpoint';
+      throw "Failed to ping authenticated endpoint";
     } catch (e) {
       rethrow;
     }
